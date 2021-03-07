26d9067aaf907648af7fae461dc91ccdaac8f936
Jack Kawell <jgkawell@outlook.com>
2021-03-07 21:20:31 +0000

    Removed node backend (good ridence tbh)

--- BackEnd/utils/convert-case.js
+++
@@ -1,9 +0,0 @@
-import jsConvertCase from 'js-convert-case';
-
-export function camelKeysArray(array) {
-  var camelArray = [];
-  for (let element of array) {
-    camelArray.push(jsConvertCase.camelKeys(element));
-  }
-  return camelArray;
-}
